3f1310cff50c2ecc03468cef126be7dd21bcf647
daisychen <daisyc2027@student.cis.edu.hk>
2024-08-27 14:24:55 +0800

    Finished first IDE quiz!

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,28 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Daisy Chen
 *****************************************************************************************************
 */
 
 public class Main {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int age;
+            String firstName;
+            String favouriteFood;
+
+            System.out.println("What is your age?");
+            age = scanner.nextInt();
+            System.out.println("What is your first name?");
+            scanner.nextLine();
+            firstName = scanner.nextLine();
+            System.out.println("What is your favourite food?");
+            favouriteFood = scanner.nextLine();
+            System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
 
 
-
-
-    }
+        }
 
 }
